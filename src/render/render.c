8f22ea767a8411426772cd4be33a9b745ecd96f4
ajnart <thomas.camlong@epitech.eu>
2020-04-15 02:05:16 +0200

    🚚 Fixing render.c missing from delivery
    Added the render.c that seemed to be missing from the delivery.

--- src/render/render.c
+++ src/render/render.c
@@ -6,6 +6,54 @@
 */
 
 #include "render.h"
+#include "../events/buttons.h"
+
+void set_button_hover(sfRenderWindow *win)
+{
+    sfVector2i mpi = sfMouse_getPositionRenderWindow(win);
+    sfVector2f mp = { .x = (float) mpi.x, .y = (float) mpi.y };
+    button_store_t *btn;
+    sfColor color;
+    char *btn_id = get_button_selected(g_buttons, mp);
+
+    if (btn_id) {
+        btn = get_button(g_buttons, btn_id);
+        color = btn->normal;
+        color.a -= 60; // ! Important / Ici je change l'aplha de mon button.
+        sfRectangleShape_setFillColor(btn->render, color);
+    }
+}
+
+void set_btn_color(sfRenderWindow *win)
+{
+    button_store_t *lookup = g_buttons;
+
+    while (lookup) {
+        sfRectangleShape_setFillColor(lookup->render, lookup->normal);
+        lookup = lookup->next;
+    }
+    set_button_hover(win);
+}
+
+void draw_buttons(sfRenderWindow *win, button_store_t *store)
+{
+    button_store_t *focus = store;
+    sfVector2f position;
+    sfFloatRect bounds;
+
+    while (focus) {
+        if (focus->render)
+            sfRenderWindow_drawRectangleShape(win, focus->render, NULL);
+        if (focus->text) {
+            bounds = sfRectangleShape_getLocalBounds(focus->render);
+            position = sfRectangleShape_getPosition(focus->render);
+            sfText_setPosition(focus->text, (sfVector2f)
+                {position.x+bounds.width/2, position.y+bounds.height/2});
+            sfRenderWindow_drawText(win, focus->text, NULL);
+        }
+        focus = focus->next;
+    }
+}
 
 sfRectangleShape *create_full_rect(sfFloatRect pos, sfTexture *tx, sfColor fill)
 {
