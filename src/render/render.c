aabe0e01b91b025179227072bb7e602ad883aff8
ajnart <thomas.camlong@epitech.eu>
2020-04-18 15:19:02 +0200

    🎨 Reworked main and render.c
    Main dosn't show the intro by default and render uses preloaded font.

--- src/render/render.c
+++ src/render/render.c
@@ -73,7 +73,6 @@ sfRectangleShape *create_full_rect(sfFloatRect pos, sfTexture *tx, sfColor fill)
 void print_message(char *str, sfRenderWindow *win, char *s_ft,
     sfVector2f info)
 {
-    sfFont *font = find_asset_byname(s_ft)->asset_store.font;
     sfFloatRect bounds;
     static sfBool init = sfFalse;
     static sfText *text;
@@ -83,12 +82,10 @@ void print_message(char *str, sfRenderWindow *win, char *s_ft,
     }
     bounds = sfText_getLocalBounds(text);
     sfText_setOrigin(text, (sfVector2f) {bounds.width/2, bounds.height * 1.2});
-    sfText_setFont(text, (const sfFont*)font);
+    sfText_setFont(text, find_asset_byname(s_ft)->asset_store.font);
     sfText_setString(text, str);
     sfText_setColor(text, sfWhite);
     sfText_setCharacterSize(text, 0.04 * sfRenderWindow_getSize(win).x);
     sfText_setPosition(text, (sfVector2f){info.x, info.y});
     sfRenderWindow_drawText(win, text, NULL);
-    if (my_strncmp(str, "cleanup", 7))
-        sfText_destroy(text);
 }
