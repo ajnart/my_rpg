04054c69e207b73a330a0458082d7101241525a2
ajnart <thomas.camlong@epitech.eu>
2020-04-18 13:34:02 +0200

    ⬆️ Reworked dependencies
    Include folder is now splitter in better filenames and should be easier to access.

---
+++ include/buttons.h
@@ -0,0 +1,44 @@
+/*
+** EPITECH PROJECT, 2019
+** MUL_my_rpg_2019
+** File description:
+** buttons.h
+*/
+
+#ifndef __MAIN_H
+#include "main.h"
+#endif
+
+#ifndef __BUTTON_H
+#define __BUTTON_H
+
+typedef struct button {
+    char *btn_id;
+    sfRectangleShape *render;
+    sfColor normal;
+    struct button *next;
+    sfText *text;
+} button_store_t;
+
+button_store_t *g_buttons;
+
+void add_button(button_store_t **store, char *id,
+    sfRectangleShape *render, char *text);
+
+char *get_button_selected(const button_store_t *store, sfVector2f loc);
+
+void destroy_buttons(button_store_t **store);
+
+void draw_buttons(sfRenderWindow *win, button_store_t *store);
+
+button_store_t *get_button(const button_store_t *store, char *name);
+
+void set_btn_color(sfRenderWindow *win);
+
+char *get_button_selected(const button_store_t *store, sfVector2f pos);
+
+void scene_btn_loader(void (*loop)(), sfRenderWindow *win);
+
+void buttons_settings(sfRenderWindow *win);
+
+#endif //__BUTTON_H
--- include/rpg.h
+++ include/rpg.h
@@ -12,17 +12,30 @@
 #include "emitter.h"
 // * Referenced bellow are the functions used for the rpg. gotten via ctags.
 
+settings_t *settings;
 
 // Number : Number of particules
 // Max : Max size
 // Range : Max range
 emitter_t *emitter_setup(int max_div, int number_div, int range_div);
 void play_music(sfRenderWindow *window, char *song, settings_t *settings);
+
+// * Main loop, calls the other loops:
+// Loops: loop_menu // loop_ingame // loop_settings
 void perform_loops(sfRenderWindow *window, void (**loop)(),
     void (**loop_old)(), event_st *state, emitter_t *emitter);
+
+// * Loop for the menu
 void loop_menu(sfRenderWindow *win, event_st *state, void (**loop)());
+// * Loop ingame
 void loop_ingame(sfRenderWindow *win, event_st *state, void (**loop)());
+// * Loop for the settings
+void loop_settings(sfRenderWindow *win, event_st *state, void (**loop)());
+
+
 void handle_events(sfEvent evt, sfRenderWindow *win, event_st *state);
 void print_message(char *str, sfRenderWindow *win, char *s_ft,
     sfVector2f info);
+sfRectangleShape *create_full_rect(sfFloatRect pos,
+    sfTexture *tx, sfColor fill);
 #endif
