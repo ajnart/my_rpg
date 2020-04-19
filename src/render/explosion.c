4a66fbaf5bbbea9a6473c5e6e243790ab5543746
ajnart <thomas.camlong@epitech.eu>
2020-04-19 14:42:22 +0200

    🚩 Added an explosion function

---
+++ src/render/explosion.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2019
+** MUL_my_rpg_2019
+** File description:
+** explosion.c
+*/
+
+#include "main.h"
+#include "emitter.h"
+#include "lib.h"
+#include "rpg.h"
+
+emitter_t *emitter_setup(int max_div, int number_div, int range_div);
+
+void explosion(sfRenderWindow *win , sfVector2f pos)
+{
+    my_printf("BOOM BOOM\n");
+    sfSprite *sprite = sfSprite_create();
+    print_message("Switching to settings...", win, "font.ttf",
+        (sfVector2f){settings->WW*0.4, settings->WH*0.7});
+    sfRenderWindow_display(win);
+    sfTime time;
+    time.microseconds = 400000;
+    sfSleep(time);
+}
