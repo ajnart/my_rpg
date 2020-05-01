f53b6234617b60c277108f38d901e895d6420a74
paul.bouzian <paul.bouzian@epitech.eu>
2020-05-01 17:00:51 +0200

    Fixed parallax code & assets

--- src/render/parallax.c
+++ src/render/parallax.c
@@ -15,7 +15,7 @@ void append_parallax(t_para **para, char *pic, int speed)
 
     element->sprite = sfSprite_create();
     element->texture = find_asset_byname(pic)->asset_store.texture;
-    element->rect = (sfIntRect){0, 0, settings->WW, settings->WH};
+    element->rect = (sfIntRect){0, 0, 1280, 720};
     element->speed = speed;
     element->next = NULL;
     sfSprite_setTexture(element->sprite, element->texture, sfFalse);
@@ -34,16 +34,16 @@ t_para *set_parallax(void)
 {
     t_para *para = NULL;
 
-    append_parallax(&para, "Foreground.png", 2);
-    append_parallax(&para, "Back2.png", 5);
-    append_parallax(&para, "Back1.png", 10);
+    append_parallax(&para, "Foreground.png", 1);
+    append_parallax(&para, "Back2.png", 2);
+    append_parallax(&para, "Back1.png", 5);
     return (para);
 }
 
 void draw_parallax(sfRenderWindow *win, t_para *para)
 {
     while (para) {
-        if (para->rect.left == settings->WW) {
+        if (para->rect.left == 1280) {
             para->rect.left = 0;
             sfSprite_setTextureRect(para->sprite, para->rect);
         } else {
